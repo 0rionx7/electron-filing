98f00c5fac4a408ff86999ad333a0724df041e6a
G.Karatzas <karatzas.geo@gmail.com>
2025-10-04 22:24:12 +0300

    Add tailwind & shadcn components

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -6,10 +6,10 @@ function App(): React.JSX.Element {
 
   return (
     <>
-      <img alt="logo" className="logo bg-stone-600" src={electronLogo} />
+      <img alt="logo" className="logo" src={electronLogo} />
       <div className="creator">Powered by electron-vite</div>
       <div className="text">
-        Build an Electron app with <span className="react">React</span>
+        Build an Electron app with <span className="react">React!!</span>
         &nbsp;and <span className="ts">TypeScript</span>
       </div>
       <p className="tip">
